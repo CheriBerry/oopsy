c3acc0109b58ced0cb8688659bddde2f2d9f592f
Berry <101909445+CheriBerry@users.noreply.github.com>
2024-09-08 17:37:54 -0400

    fixed a thinga

--- init.lua
+++ init.lua
@@ -1,22 +1,20 @@
 return function(parents, methods)
-    local methods_
-    if methods then methods_ = methods else methods_ = parents end
-    local class_ = methods_
-	local instance = {}
-	instance.init = class_.init
-    instance.methods = {}
+	local methods_
+	if methods then methods_ = methods else methods_ = parents end
+	local class_ = methods_
+	local instance = methods
 	for i,parent in ipairs(parents) do
-		for name,method in pairs(parent.methods) do
+		for name,method in pairs(parent) do
 			if name ~= "init" then
-				instance.methods[name] = method
-                class_[name] = method
+				instance[name] = method
+				class_[name] = method
 			end
 		end
 	end
-    for name,method in pairs(methods_) do
-        instance.methods[name] = method
-        class_[name] = method
-    end
+	for name,method in pairs(methods_) do
+		instance[name] = method
+		class_[name] = method
+	end
 	function instance:new(...)
 		class_:init(...)
 		return class_
