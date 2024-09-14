5148503a5f3eadff12cbbc921f2e907bd78ed1f7
Berry <101909445+CheriBerry@users.noreply.github.com>
2024-09-14 15:03:45 -0400

    not sure how i fixed it but i fixed it :P

--- init.lua
+++ init.lua
@@ -1,24 +1,33 @@
 return function(parents, methods)
-	local methods_
-	if methods then methods_ = methods else methods_ = parents end
-	local class_ = methods_
-	local instance = methods
-	for i,parent in ipairs(parents) do
-		for name,method in pairs(parent) do
-			if name ~= "init" then
-				instance[name] = method
-				class_[name] = method
-			end
-		end
-	end
-	for name,method in pairs(methods_) do
-		instance[name] = method
-		class_[name] = method
-	end
-	function instance:new(...)
-		class_:init(...)
-		return class_
-	end
-	instance = setmetatable(instance, {__call=instance.new})
-	return instance
+    local methods_ = methods or parents
+    local class_ = methods_
+    local instance = {}
+    
+    -- Copy methods from parents to instance and class_
+    for _, parent in ipairs(parents) do
+        for name, method in pairs(parent) do
+            if name ~= "init" then
+                instance[name] = method
+                class_[name] = method
+            end
+        end
+    end
+    
+    -- Copy methods from methods_ to instance and class_
+    for name, method in pairs(methods_) do
+        instance[name] = method
+        class_[name] = method
+    end
+
+    -- Define the `new` method
+    function instance:new(...)
+        local obj = setmetatable({}, {__index = class_})
+        if class_.init then
+            class_.init(obj, ...)
+        end
+        return obj
+    end
+
+    -- Set the metatable for instance to allow calling as a constructor
+    return setmetatable(instance, {__call = instance.new})
 end
